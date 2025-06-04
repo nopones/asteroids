c6847b9bb007925ba9ec851b37b163d042a0bcc0
nopones <macdi19@gmail.com>
2025-06-03 20:54:27 -0600

    Added files

---
+++ main.py
@@ -0,0 +1,30 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import (
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    ASTEROID_MIN_RADIUS,
+    ASTEROID_MAX_RADIUS,
+    ASTEROID_KINDS,
+    ASTEROID_SPAWN_RATE,
+)
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print (f"Screen width: {SCREEN_WIDTH}")
+    print (f"Screen height: {SCREEN_HEIGHT}")
+    black= (0,0,0)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(black)
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
