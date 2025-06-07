81e1fde98c731702fab9265b520ccfd895cf6730
nopones <macdi19@gmail.com>
2025-06-07 16:57:20 -0600

    Optimize CPU and exit option

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ from constants import (
     ASTEROID_MAX_RADIUS,
     ASTEROID_KINDS,
     ASTEROID_SPAWN_RATE,
-)
-
+)    
 
 def main():
     pygame.init()
@@ -18,6 +17,9 @@ def main():
     print("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
+    clock=pygame.time.Clock()
+    dt=0
+
     black= (0,0,0)
     while True:
         for event in pygame.event.get():
@@ -25,6 +27,8 @@ def main():
                 return
         screen.fill(black)
         pygame.display.flip()
+        dt=clock.tick(60)/1000
+        #dt=(pygame.tick(60)/1000)
 
 if __name__ == "__main__":
     main()
