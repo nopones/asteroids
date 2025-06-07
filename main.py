d52eb2a8c42c5125789f6677c268558c5c4a0e69
nopones <macdi19@gmail.com>
2025-06-07 17:46:15 -0600

    Added player graphics

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import Player
 from constants import (
     SCREEN_WIDTH,
     SCREEN_HEIGHT,
+    PLAYER_RADIUS,
     ASTEROID_MIN_RADIUS,
     ASTEROID_MAX_RADIUS,
     ASTEROID_KINDS,
@@ -14,6 +16,7 @@ from constants import (
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player=Player(x = SCREEN_WIDTH / 2,y = SCREEN_HEIGHT / 2)
     print("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
@@ -26,8 +29,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(black)
+        player.draw(screen)
         pygame.display.flip()
         dt=clock.tick(60)/1000
+        
         #dt=(pygame.tick(60)/1000)
 
 if __name__ == "__main__":
